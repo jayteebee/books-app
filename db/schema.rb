8f32409a2616a33209612d8d1d62e511fd448dff
Jethro_Block <jethroblock22@gmail.COM>
2023-05-23 20:42:33 +0100

    book made ref made

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_23_193636) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_23_194134) do
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -20,4 +20,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_193636) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "books", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  add_foreign_key "books", "authors"
 end
