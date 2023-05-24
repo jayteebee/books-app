c5d5d81a54de72748e91b9ad479171d0423dceb5
Jethro_Block <jethroblock22@gmail.COM>
2023-05-24 19:19:02 +0100

    Complete

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,5 +4,33 @@ class AuthorsController < ApplicationController
       @authors = Author.all
     end
 
+    def show
+        @author = Author.find(params[:id])
+        @books = @author.books
+    end
 
+    def new
+      @author = Author.new
+    end
+
+    def create
+        Author.create(params.require(:author).permit(:first_name, :last_name, :number_of_books, :age))
+        redirect_to authors_path
+    end
+
+    def edit
+      @author = Author.find(params[:id])
+    end
+
+    def update
+      @author = Author.find(params[:id])
+      @author.update(params.require(:author).permit(:first_name, :last_name, :number_of_books, :age))
+        redirect_to authors_path
+    end
+
+    def destroy
+      @author = Author.find(params[:id])
+      @author.destroy
+      redirect_to authors_path
+    end
 end
