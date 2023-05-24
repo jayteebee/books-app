f646c6056af5f8cf12f4cf24582499d72ef4ed04
Jethro_Block <jethroblock22@gmail.COM>
2023-05-24 19:19:11 +0100

    Done!

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
   root "application#index"
 
   get "authors", to: "authors#index"
+  get "authors/new", to: "authors#new", as: :new_author
+  get "authors/:id", to: "authors#show", as: :author
+  post "authors", to: "authors#create"
+  get "authors/:id/edit", to: "authors#edit", as: :edit_author
+  patch "authors/:id", to: "authors#update"
+  delete "authors/:id", to: "authors#destroy"
+
+  resources :authors
 end
